45ecc3f45ab9b12aa72af62a01b643da976dd949
Sakari Kapanen <sakari.m.kapanen@gmail.com>
2017-02-05 23:45:55 +0200

    GPLv3 headers

--- user/user_main.c
+++ user/user_main.c
@@ -1,6 +1,6 @@
 /*
     Firmware for a segway-style robot using ESP8266.
-    Copyright (C) 201&  Sakari Kapanen
+    Copyright (C) 2017  Sakari Kapanen
 
     This program is free software: you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
