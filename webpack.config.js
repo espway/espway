b440b12760862fd0584e5f06d132b2525ade6d39
Sakari Kapanen <sakari.m.kapanen@gmail.com>
2017-04-17 11:41:39 +0300

    Fix the webpack dev server path

--- webpack.config.js
+++ webpack.config.js
@@ -39,7 +39,7 @@ module.exports = {
     },
 
     devServer: {
-        contentBase: path.resolve(__dirname, 'html')
+        contentBase: path.resolve(__dirname, 'espway-arduino/data')
     }
 };
 
