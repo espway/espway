e5a8d9a03a9ed8219eaced8770eb74c5b688595c
Sakari Kapanen <sakari.m.kapanen@gmail.com>
2017-08-30 12:18:52 +0300

    A working version with new LwIP

--- src/lib/pwm.c
+++ src/lib/pwm.c
@@ -192,11 +192,7 @@ pwm_init(uint32_t period, uint32_t *duty, uint32_t pwm_channel_num,
 
 	pwm_set_period(period);
 
-#ifdef ESPWAY_DEVEL
 	_xt_isr_attach(INUM_TIMER_FRC1, (_xt_isr)pwm_intr_handler, NULL);
-#else
-	_xt_isr_attach(INUM_TIMER_FRC1, (_xt_isr)pwm_intr_handler);
-#endif
 	TM1_EDGE_INT_ENABLE();
 
 	timer->frc1_int &= ~FRC1_INT_CLR_MASK;
