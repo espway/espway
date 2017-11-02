6403cfd6e21607fe2d0d915e972c2c479be55294
Sakari Kapanen <sakari.m.kapanen@gmail.com>
2017-11-02 14:51:02 +0200

    Add comments to q16_rsqrt

--- src/lib/q16.c
+++ src/lib/q16.c
@@ -85,12 +85,52 @@ q16 q16_div(q16 a, q16 b)
 
 q16 q16_rsqrt(q16 x)
 {
+  /* A Q16.16 fixed point approximation of the reciprocal square root.
+   *
+   * Let the number x be represented as follows:
+   *   x = m * 2^2n,
+   * where m is real in [1,4[, and n is integer. Then
+   *   1 / sqrt(x) = 1 / sqrt(m * 2^n) = 1 / (sqrt(m) * 2^n)
+   * = 1 / sqrt(m) * 2^-n.
+   *
+   * Thus it is sufficient to use a look-up table of 1 / sqrt(m) values for
+   * m in the interval [1, 4[. The power calculations in fixed point are easily
+   * implemented using shifts.
+   */
+
+  /* To calculate the (even) power, we count leading zeros, clear the odd bit
+   * and subtract the value from 14. E.g. the Q16.16 representation of number 2
+   * is 1 << 17 and has thus 17 trailing and 14 leading zeros.
+   */
   int power = 14 - (__builtin_clz(x) & ~0x01);
 
-  q16 x_normalized = x >> (power + 10);
+  /* The look-up table is indexed such that indices for [1,4[ are in the
+   * interval [0, 192[. Hence we normalize x to that interval by first
+   * dividing by 2^2n, then shifting right by 10 (thus 1<<17 becomes 1<<7) to
+   * bring it to the range [0, 256[ and finally subtract 64 to bring it down to
+   * the correct index range.*/
+  size_t i = (x >> (power + 10)) - 64;
 
-  size_t i = x_normalized - 64;
+  /* The Q16.16 values of 1 / sqrt(m) in the LUT are scaled by 0.5, so we
+   * cancel that here by adding 1 to the power.
+   */
   q16 y = Q16_RSQRT_LUT[i] << (-(power / 2) + 1);
+
+  /* So far we have obtained a good estimate. Let's improve it a little by some
+   * iteration. We are solving the unknown y from the equation
+   *   y = 1/sqrt(x),
+   * or equivalently,
+   *   f(y) = 1 / y^2 - x = 0.
+   * Differentiating by y, we get
+   *   f'(y) = -2 / y^3.
+   * Then the Newton-Raphson iteration can be applied as follows:
+   *   y_(n+1) = y_n - f(y) / f'(y)
+   *           = y_n - (1 / y_n^2 - x) / (-2 / y_n^3)
+   *           = y_n - (y_n - y_n^3 * x) / (-2)
+   *           = y_n * (3/2 - y_n^2 * x)
+   * 
+   * The equivalent form is used to avoid division in the iteration equation.
+   */
   q16 y2 = q16_mul(y, y);
   y = q16_mul(y, 3*Q16_ONE - q16_mul(x, y2)) / 2;
   y2 = q16_mul(y, y);
@@ -98,3 +138,4 @@ q16 q16_rsqrt(q16 x)
 
   return y;
 }
+
